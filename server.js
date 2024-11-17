9a734862e6dc072ae51a8d1432e322d139fd409d
AnteVol <ante.volarevic@fer.hr>
2024-11-17 16:08:47 +0100

    Added public folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./'));
+app.use(express.static('./public'));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
